d5d54256b6c4dcef978285e6998eed26e4388e8c
Maksym <zzzeleznyakmaxim@gmail.com>
2025-08-01 13:43:33 +0300

    report owner

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
- 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+if (!apiUrl) {
+  throw new Error("NEXT_PUBLIC_API_URL is not defined. Додайте цю змінну у .env.local");
+}
+
+const baseURL = apiUrl + "/api";
+
 export const api = axios.create({
   baseURL,
   withCredentials: true,
-}); 
+});
